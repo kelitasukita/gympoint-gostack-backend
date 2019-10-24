f89a6985c2052ce54fbae0df55bac979752461e9
kelitasukita <kelitasukita@gmail.com>
2019-10-24 16:52:41 +0200

    Models e migrations dos alunos

---
+++ src/database/migrations/20191024132857-create-students.js
@@ -0,0 +1,43 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('students', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      idade: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      peso: {
+        allowNull: false,
+        type: Sequelize.DECIMAL
+      },
+      altura: {
+        allowNull: false,
+        type: Sequelize.DECIMAL
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: queryInterface => {
+    return queryInterface.dropTable('students');
+  }
+};
