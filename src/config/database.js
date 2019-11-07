de9ff5fe73cc5039302dfc65e488ab06fbab9b1b
kelitasukita <kelitasukita@gmail.com>
2019-11-07 20:14:21 +0100

    Alteração do nome da base de dados

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ module.exports = {
   host: 'localhost',
   username: 'postgres',
   password: 'docker',
-  database: 'gympoint',
+  database: 'gympoint-gostack',
   define: {
     timeStamps: true,
     underscored: true,
