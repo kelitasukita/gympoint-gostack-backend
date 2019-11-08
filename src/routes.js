fac4e2fb4212c489f6832231ea8763153db4d955
kelitasukita <kelitasukita@gmail.com>
2019-11-08 18:08:45 +0100

    CRUD da tabela 'planos'

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import SessionController from './app/controllers/SessionController';
 import StudentsController from './app/controllers/StudentsController';
+import PlanController from './app/controllers/PlanController';
 
 import authMiddleware from './app/middlewares/middleAuth';
 
@@ -13,5 +14,10 @@ rotas.use(authMiddleware);
 
 rotas.post('/students', StudentsController.store);
 rotas.put('/students/:id', StudentsController.updade);
+
+rotas.post('/plans', PlanController.store);
+rotas.get('/plans', PlanController.index);
+rotas.put('/plans/:id', PlanController.update);
+rotas.delete('/plans/:id', PlanController.delete);
 
 export default rotas;
